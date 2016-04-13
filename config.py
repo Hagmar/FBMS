36dcd849e1ce40f6f0f6c1a69bd1da9426446396
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-14 02:05:04 +1000

    Added configuration fields. Can probably be reduced

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
 cookie = 'XXXXXXXXXX'
+
+# This can probably be reduced
+request_data = {
+    '__user'    : '<your Facebook id>',
+    '__a'       : '<your __a>',
+    '__dyn'     : '<your __dyn>',
+    '__req'     : '<your __req>',
+    '__be'      : '<your __be>',
+    '__pc'      : '<your __pc>',
+    'fb_dtsg'   : '<your fb_dtsg>',
+    'ttstamp'   : '<your ttstamp>',
+    '__rev'     : '<your __rev>'
+    }
